d256acfeedb939fbc0e7684849854d745125b805
janez.baloniet@gmail.com <janez.baloniet@gmail.com>
2016-12-17 10:06:52 +0100

    RC1

--- common/models/a-person.js
+++ common/models/a-person.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = function(Aperson) {
-
+    Aperson.disableRemoteMethod('__get__person', false);
+    Aperson.disableRemoteMethod('__get__activity', false);
 };
--- common/models/p-edu.js
+++ common/models/p-edu.js
@@ -1,5 +1,13 @@
 'use strict';
 
-module.exports = function(Pedu) {
+module.exports = function (Pedu) {
+
+    Pedu.disableRemoteMethod('__create__education', false);
+    Pedu.disableRemoteMethod('__delete__education', false);
+    Pedu.disableRemoteMethod('__count__education', false);
+    Pedu.disableRemoteMethod('__findById__education', false);
+    Pedu.disableRemoteMethod('__get__education', false);
+    Pedu.disableRemoteMethod('__updateById__education', false);
+    Pedu.disableRemoteMethod('__destroyById__education', false);
 
 };
