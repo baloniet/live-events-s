683071e486b6bbd5f65a2d4ceeb3638afdfc0e18
Lavric Janez <Lavric_Janez@rtvslo.si>
2017-01-23 15:32:58 +0100

    RC4

--- common/models/v-mevent-e.js
+++ common/models/v-mevent-e.js
@@ -1,5 +1,34 @@
 'use strict';
 
-module.exports = function(Vmevente) {
+module.exports = function (Vmevente) {
+    Vmevente.people = function (id, cb) {
+        var ds = Vmevente.dataSource;
+        var sql = "SELECT DISTINCT person_id, firstname, lastname FROM v_mevent_e " +
+        "where (eAcc = 1 and (eOff is null or eOff = 0) ) and  location_id=" + id + " order by lastname, firstname";
+        ds.connector.execute(sql, function (err, people) {
+            if (err)
+                console.error(err);
+            cb(err, people);
+        });
+    }
+
+    Vmevente.remoteMethod('people', {
+    http: {
+      verb: 'get',
+      path: '/:id/people'
+    },
+    accepts: {
+      arg: 'id',
+      type: 'number',
+      required: true,
+      description: 'location id'
+    },
+    returns: {
+      type: 'Object',
+      root: true
+    },
+    description: 'Get registered people of location',
+    notes: 'This custom method returns registered people of provided location id.'
+  });
 
 };
