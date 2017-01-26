9e29baea6591e8e539b164034892f4e948fc9db8
janez.baloniet@gmail.com <janez.baloniet@gmail.com>
2017-01-26 21:39:59 +0100

    RC5

--- common/models/p-emp.js
+++ common/models/p-emp.js
@@ -1,5 +1,12 @@
 'use strict';
 
-module.exports = function(Pemp) {
+module.exports = function (Pemp) {
+  Pemp.disableRemoteMethod('__create__employment', false);
+  Pemp.disableRemoteMethod('__delete__employment', false);
+  Pemp.disableRemoteMethod('__count__employment', false);
+  Pemp.disableRemoteMethod('__findById__employment', false);
+  Pemp.disableRemoteMethod('__get__employment', false);
+  Pemp.disableRemoteMethod('__updateById__employment', false);
+  Pemp.disableRemoteMethod('__destroyById__employment', false);
 
 };
--- common/models/person.js
+++ common/models/person.js
@@ -1,53 +1,60 @@
 'use strict';
 
 module.exports = function (Person) {
-    Person.disableRemoteMethod('__create__phones', false);
-    Person.disableRemoteMethod('__delete__phones', false);
-    Person.disableRemoteMethod('__count__phones', false);
-    Person.disableRemoteMethod('__findById__phones', false);
-    Person.disableRemoteMethod('__updateById__phones', false);
-    Person.disableRemoteMethod('__destroyById__phones', false);
+  Person.disableRemoteMethod('__create__phones', false);
+  Person.disableRemoteMethod('__delete__phones', false);
+  Person.disableRemoteMethod('__count__phones', false);
+  Person.disableRemoteMethod('__findById__phones', false);
+  Person.disableRemoteMethod('__updateById__phones', false);
+  Person.disableRemoteMethod('__destroyById__phones', false);
 
-    Person.disableRemoteMethod('__create__emails', false);
-    Person.disableRemoteMethod('__delete__emails', false);
-    Person.disableRemoteMethod('__count__emails', false);
-    Person.disableRemoteMethod('__findById__emails', false);
-    Person.disableRemoteMethod('__updateById__emails', false);
-    Person.disableRemoteMethod('__destroyById__emails', false);
+  Person.disableRemoteMethod('__create__emails', false);
+  Person.disableRemoteMethod('__delete__emails', false);
+  Person.disableRemoteMethod('__count__emails', false);
+  Person.disableRemoteMethod('__findById__emails', false);
+  Person.disableRemoteMethod('__updateById__emails', false);
+  Person.disableRemoteMethod('__destroyById__emails', false);
 
-    Person.disableRemoteMethod('__create__edu', false);
-    Person.disableRemoteMethod('__delete__edu', false);
-    Person.disableRemoteMethod('__count__edu', false);
-    Person.disableRemoteMethod('__findById__edu', false);
-    Person.disableRemoteMethod('__updateById__edu', false);
-    Person.disableRemoteMethod('__destroyById__edu', false);
+  Person.disableRemoteMethod('__create__edu', false);
+  Person.disableRemoteMethod('__delete__edu', false);
+  Person.disableRemoteMethod('__count__edu', false);
+  Person.disableRemoteMethod('__findById__edu', false);
+  Person.disableRemoteMethod('__updateById__edu', false);
+  Person.disableRemoteMethod('__destroyById__edu', false);
 
-    Person.disableRemoteMethod('__create__citi', false);
-    Person.disableRemoteMethod('__delete__citi', false);
-    Person.disableRemoteMethod('__count__citi', false);
-    Person.disableRemoteMethod('__findById__citi', false);
-    Person.disableRemoteMethod('__updateById__citi', false);
-    Person.disableRemoteMethod('__destroyById__citi', false);
+  Person.disableRemoteMethod('__create__emp', false);
+  Person.disableRemoteMethod('__delete__emp', false);
+  Person.disableRemoteMethod('__count__emp', false);
+  Person.disableRemoteMethod('__findById__emp', false);
+  Person.disableRemoteMethod('__updateById__emp', false);
+  Person.disableRemoteMethod('__destroyById__emp', false);
 
-    Person.disableRemoteMethod('__create__addss', false);
-    Person.disableRemoteMethod('__delete__addss', false);
-    Person.disableRemoteMethod('__count__addss', false);
-    Person.disableRemoteMethod('__findById__addss', false);
-    Person.disableRemoteMethod('__updateById__addss', false);
-    Person.disableRemoteMethod('__destroyById__addss', false);
+  Person.disableRemoteMethod('__create__citi', false);
+  Person.disableRemoteMethod('__delete__citi', false);
+  Person.disableRemoteMethod('__count__citi', false);
+  Person.disableRemoteMethod('__findById__citi', false);
+  Person.disableRemoteMethod('__updateById__citi', false);
+  Person.disableRemoteMethod('__destroyById__citi', false);
 
-    Person.disableRemoteMethod('__create__statements', false);
-    Person.disableRemoteMethod('__delete__statements', false);
-    Person.disableRemoteMethod('__count__statements', false);
-    Person.disableRemoteMethod('__findById__statements', false);
-    Person.disableRemoteMethod('__updateById__statements', false);
-    Person.disableRemoteMethod('__destroyById__statements', false);
+  Person.disableRemoteMethod('__create__addss', false);
+  Person.disableRemoteMethod('__delete__addss', false);
+  Person.disableRemoteMethod('__count__addss', false);
+  Person.disableRemoteMethod('__findById__addss', false);
+  Person.disableRemoteMethod('__updateById__addss', false);
+  Person.disableRemoteMethod('__destroyById__addss', false);
 
-    Person.disableRemoteMethod('__create__stats', false);
-    Person.disableRemoteMethod('__delete__stats', false);
-    Person.disableRemoteMethod('__count__stats', false);
-    Person.disableRemoteMethod('__findById__stats', false);
-    Person.disableRemoteMethod('__updateById__stats', false);
-    Person.disableRemoteMethod('__destroyById__stats', false);
+  Person.disableRemoteMethod('__create__statements', false);
+  Person.disableRemoteMethod('__delete__statements', false);
+  Person.disableRemoteMethod('__count__statements', false);
+  Person.disableRemoteMethod('__findById__statements', false);
+  Person.disableRemoteMethod('__updateById__statements', false);
+  Person.disableRemoteMethod('__destroyById__statements', false);
+
+  Person.disableRemoteMethod('__create__stats', false);
+  Person.disableRemoteMethod('__delete__stats', false);
+  Person.disableRemoteMethod('__count__stats', false);
+  Person.disableRemoteMethod('__findById__stats', false);
+  Person.disableRemoteMethod('__updateById__stats', false);
+  Person.disableRemoteMethod('__destroyById__stats', false);
 
 };
