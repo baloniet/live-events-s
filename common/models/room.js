453389a1a96650c18183137a5d54248860f29343
janez.baloniet@gmail.com <janez.baloniet@gmail.com>
2017-04-08 14:13:06 +0200

    RC7

--- common/models/room.js
+++ common/models/room.js
@@ -1,5 +1,41 @@
 'use strict';
 
-module.exports = function(Room) {
+module.exports = function (Room) {
+  Room.isRoomFree = function (id, starttime, endtime, cb) {
+    var ds = Room.dataSource;
+    var sql = "select isroomfree(" + id + "," + starttime + "," + endtime + ")";
+    ds.connector.execute(sql, function (err, isRoomFree) {
+      if (err)
+        console.error(err);
+      cb(err, isRoomFree);
+    });
+  };
 
+  Room.remoteMethod('isRoomFree', {
+    http: {
+      verb: 'post',
+      path: '/:id/isRoomFree'
+    },
+    accepts: [{
+      arg: 'id',
+      type: 'number',
+      required: true,
+      description: 'Room id'
+    }, {
+      arg: 'starttime',
+      type: 'string',
+      required: true,
+      description: 'start time'
+    },{
+      arg: 'endtime',
+      type: 'string',
+      required: true,
+      description: 'end time'
+    }],
+    returns: {
+      type: 'Object',
+      root: true
+    },
+    description: 'Check if room is free'
+  });
 };
